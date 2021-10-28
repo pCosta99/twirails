8b64ee018f21c17d523e9b38e518e33b347669ca
Pedro Costa <costapedro.a1999@gmail.com>
2021-10-29 00:21:45 +0100

    done with followings

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
 
   has_many :tweets, dependent: :destroy
   has_many :followers, dependent: :destroy, foreign_key: 'followee_id'
+
+  def list_of_followers
+    Follower.where(followee_id: id).map do |f|
+      User.find(f.follower_id)
+    end
+  end
+
+  def list_of_following
+    Follower.where(follower_id: id).map do |f|
+      User.find(f.followee_id)
+    end
+  end
 end
