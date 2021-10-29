0057f823f49d18bed35d63f7117bb05fd9b8771f
Pedro Costa <costapedro.a1999@gmail.com>
2021-10-29 19:23:07 +0100

    like/dislike as a put

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: %i[show index] do
     member do
-      get 'follow'
-      get 'unfollow'
+      put 'follow'
+      put 'unfollow'
     end
     resources :tweets
   end
