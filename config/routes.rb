fcaaf0f46b044ba76e078d368774db345e360be1
Pedro Costa <costapedro.a1999@gmail.com>
2021-10-29 22:07:45 +0100

    fixed sign out issue

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   put '/tweets/:id/like', to: 'tweets#like', as: 'tweet_like'
   put '/tweets/:id/dislike', to: 'tweets#dislike', as: 'tweet_dislike'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   resources :users, only: %i[show index] do
     member do
       put 'follow'
