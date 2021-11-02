277e17369e56bd411ac16a2589124337b9c616e2
Pedro Costa <costapedro.a1999@gmail.com>
2021-11-02 18:11:00 +0000

    first attempt at deploying

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Users
+users = User.create([{ email: 'a@gmail.com', password: 'password' },
+                     { email: 'b@gmail.com', password: 'password' },
+                     { email: 'c@gmail.com', password: 'password' },
+                     { email: 'd@gmail.com', password: 'password' },
+                     { email: 'e@gmail.com', password: 'password' },
+                     { email: 'f@gmail.com', password: 'password' }])
